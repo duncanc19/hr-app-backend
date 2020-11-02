26ec9512baf76d85c416a6783a6a347b323b1603
nax2uk <azlinayeo@gmail.com>
2020-11-02 10:32:09 +0000

    added test for post request for login endpoint

--- tests/acceptance_tests.cs
+++ tests/acceptance_tests.cs
@@ -20,5 +20,28 @@ namespace tests
 
             Assert.Equal("This is the home page! Isn't it great!", stringResponse);
         }
+
+        [Fact]
+        public async Task PostUsernameAndPasswordEndpoint()
+        {
+            var apiClient = new HttpClient();
+            var user = new User
+            {
+                Username = "Duncan",
+                Password = "abc" 
+            };
+            // Serialize our concrete class into a JSON String
+            var stringUser = await Task.Run(() => JsonConvert.SerializeObject(user));
+            // Wrap our JSON inside a StringContent which then can be used by the HttpClient class
+            var httpContent = new StringContent(stringUser, Encoding.UTF8, "application/json");
+
+            var apiResponse = await apiClient.PostAsync($"http://localhost:5003/api/login", httpContent);
+
+            var stringResponse = await apiResponse.Content.ReadAsStringAsync();
+
+            // returns userid, name, role and permissions
+            Assert.Equal("true", stringResponse);
+        }
+
     }
 }
