01c00798e553ec6c5dae6f42b4c868e839657911
duncanc19 <duncancarter93@gmail.com>
2020-11-03 15:52:53 +0000

    added tests for ignoring unexpected fields

--- tests/UserControllerTests.cs
+++ tests/UserControllerTests.cs
@@ -174,6 +174,33 @@ namespace tests
             // Console.WriteLine(jsonResponse.ToString());
             Assert.Equal(expectedResponse, jsonResponse);
         }
+
+             [Fact]
+        public async Task PutUserEndpointWithInvalidField()
+        {
+            // Arrange
+            var apiClient = new HttpClient();
+            var change = JToken.FromObject(new { Pets = "Two cats" });
+
+            // Serialize our concrete class into a JSON String
+            var stringChange = await Task.Run(() => JsonConvert.SerializeObject(change));
+            
+            // Wrap our JSON inside a StringContent which then can be used by the HttpClient class
+            var httpContent = new StringContent(stringChange, Encoding.UTF8, "application/json");
+            var userId = "18712a4f-744e-4e7c-a191-395fa832518b";
+
+            var expectedResponse = JToken.FromObject(new { FirstName = "Azlina", Surname = "Yeo", Role = "Employee", PermissionLevel = "Default",
+                        Telephone = "0771333546433", Email = "azlina@happy.com", Location = "Singapore", NextOfKin = "Father", Address = "Bedok Reservoir Road",
+                        Salary = "£29000", DoB = new DateTime(1979,01,01) });
+
+            var apiResponse = await apiClient.PutAsync($"http://localhost:5003/api/user/{userId}", httpContent);
+            // Assert
+            Assert.True(apiResponse.IsSuccessStatusCode);
+
+            var jsonResponse = JToken.Parse(await apiResponse.Content.ReadAsStringAsync());
+            // Console.WriteLine(jsonResponse.ToString());
+            Assert.Equal(expectedResponse, jsonResponse);
+        }
     }
 }
     
