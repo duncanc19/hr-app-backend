02e8ab3d84ae4cc9c4370eb5c1307d8f8c0cd9ac
nax2uk <azlinayeo@gmail.com>
2020-11-03 09:50:04 +0000

    2nd commit got GET request on user endpoint

---
+++ api/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HRApp.API.Models;
+
+namespace HRApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        List<User> Users { get; set; }
+
+       public UserController()
+       {
+           var userList = new UserList();
+           Users = userList.users;
+       }
+
+        // GET api/user
+        [HttpGet]
+        public ActionResult<UserInfo> Get(Guid id)
+        {
+            var userfound = Users.SingleOrDefault( x => x.Id == id );
+            if (userfound != null)
+            {
+                return Ok(userfound.UserInfo); 
+            }
+            return BadRequest(new {message = "Id does not exist"});
+            
+        }
+    }
+}
