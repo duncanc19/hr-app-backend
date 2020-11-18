f3650835cc75723a69fc6e9b05f837e6737b9850
Richard Pentecost <richard.pentecost@madetech.com>
2020-11-18 14:25:02 +0000

    changed database string

--- api/Program.cs
+++ api/Program.cs
@@ -13,7 +13,7 @@ namespace api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().MigrateDatabase().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
