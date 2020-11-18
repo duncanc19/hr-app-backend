ce29d36be650399131726be923d52b8ac3ca2e58
Richard Pentecost <richard.pentecost@madetech.com>
2020-11-18 12:03:54 +0000

    Extensions.cs added

--- api/Program.cs
+++ api/Program.cs
@@ -13,7 +13,7 @@ namespace api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().MigrateDatabase().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
