881fa12976ced1feaf1bdc59ae7117142f7eedaf
joannafawl <joanna.fawl@madetech.com>
2020-11-23 15:08:04 +0000

    Add and pass test for getting a visit using the ID

--- tests/AcceptanceTests/VisitorAcceptanceTests.cs
+++ tests/AcceptanceTests/VisitorAcceptanceTests.cs
@@ -46,9 +46,38 @@ namespace tests
             // Assert
             foreach (var field in expectedResponse)
             {
-                Console.WriteLine(expectedResponse[field.Key]);
-                Console.WriteLine(jsonResponse["visitor"][field.Key]);
-                Assert.Equal(expectedResponse[field.Key], jsonResponse["visitor"][field.Key]);
+                Assert.Equal(expectedResponse[field.Key], jsonResponse["visit"][field.Key]);
+            }
+        }
+
+        [Fact]
+          public async Task GetVisitEndpointWithId()
+        {
+            // Arrange
+            var apiClient = new HttpClient();
+
+            
+            var visitId = "0aa862e4-68ee-45ed-873e-ee635f0b47e5";
+            var expectedResponse = new Dictionary<string, string>(){
+                {"firstName", "John"},
+                {"surname", "Doe"},
+                {"company", "Disneyland"},
+                {"role", "Marketing"},
+                {"telephone", "07716354633"},
+                {"email", "johnd@disneyland.com"},
+                {"employeeEmail", "jfawl@skillsforcare.org"}
+                // {"appointment", $"{new DateTime(2020,12,01,12,30,00).ToString("dd/MM/yyyy HH:mm:ss")}"}
+            };
+
+            // Act
+            
+            var apiResponse = await apiClient.GetAsync($"http://localhost:5003/api/visitor/{visitId}" );
+            var jsonResponse = JToken.Parse(await apiResponse.Content.ReadAsStringAsync());
+            
+            // Assert
+            foreach (var field in expectedResponse)
+            {
+                Assert.Equal(expectedResponse[field.Key], jsonResponse["visit"][field.Key]);
             }
         }
     }
