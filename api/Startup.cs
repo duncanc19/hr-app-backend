5a56f5bf5d011a228c27a67e7ffc9b28847cfd90
Richard Pentecost <richard.pentecost@madetech.com>
2020-11-24 10:24:10 +0000

    alter connection string with environment variables

--- api/Startup.cs
+++ api/Startup.cs
@@ -19,6 +19,7 @@ using HRApp.API.Helpers;
 using HRApp.API.IServices;
 using HRApp.API.Services;
 using HRApp.API.Models;
+using Npgsql;
 
 namespace api
 {
@@ -36,7 +37,7 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // // services.AddCors("AllowAnyOrigin", builder => builder.AllowAnyOrigin());
+            // services.AddCors("AllowAnyOrigin", builder => builder.AllowAnyOrigin());
             services.AddCors(options =>
             {
             options.AddPolicy( "AllowOrigin",
@@ -79,33 +80,59 @@ namespace api
             
             string connStr;
             string databaseString = Environment.GetEnvironmentVariable("DATABASE_URL");
+            // string databaseString = "postgres://qqylwlibbqzfnt:4712d6c25092370e4e5fc34286c1c15c143a1039ec52562b7d79d51731cbf483@ec2-34-232-147-86.compute-1.amazonaws.com:5432/d97hs6fcue95i0";
             if (string.IsNullOrEmpty(databaseString))
             {
                 // Use connection string from file.
-                connStr = "DefaultConnection1";
+                connStr = Environment.GetEnvironmentVariable("DefaultConnection1");
             }
             else
             {
-                // Use connection string provided at runtime by Heroku.
-                var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
+                // var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                var databaseUri = new Uri(databaseString);
+                Console.WriteLine("*******************");
+                Console.WriteLine(databaseUri);
+                var userInfo = databaseUri.UserInfo.Split(':');
+
+                var builder = new NpgsqlConnectionStringBuilder
+                {
+                    Host = databaseUri.Host,
+                    Port = databaseUri.Port,
+                    Username = userInfo[0],
+                    Password = userInfo[1],
+                    Database = databaseUri.LocalPath.TrimStart('/')
+                };
+
+                connStr = builder.ToString();
+                
+                
                 // Parse connection URL to connection string for Npgsql
-                connUrl = connUrl.Replace("postgres://", string.Empty);
-                var pgUserPass = connUrl.Split("@")[0];
-                var pgHostPortDb = connUrl.Split("@")[1];
-                var pgHostPort = pgHostPortDb.Split("/")[0];
-                var pgDb = pgHostPortDb.Split("/")[1];
-                var pgUser = pgUserPass.Split(":")[0];
-                var pgPass = pgUserPass.Split(":")[1];
-                var pgHost = pgHostPort.Split(":")[0];
-                var pgPort = pgHostPort.Split(":")[1];
+                // databaseString =  databaseString.Replace("postgres://", string.Empty);
+                // var pgUserPass =    databaseString.Split("@")[0];
+                // var pgHostPortDb =  databaseString.Split("@")[1];
+                // var pgHostPort = pgHostPortDb.Split("/")[0];
+                // var pgDb = pgHostPortDb.Split("/")[1];
+                // var pgUser = pgUserPass.Split(":")[0];
+                // var pgPass = pgUserPass.Split(":")[1];
+                // var pgHost = pgHostPort.Split(":")[0];
+                // var pgPort = pgHostPort.Split(":")[1];
 
-                connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
-        }
+                // connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+                // "Server=ec2-34-232-147-86.compute-1.amazonaws.com;"
+                // "Port=5432;Username=qqylwlibbqzfnt;"
+                // "Password=4712d6c25092370e4e5fc34286c1c15c143a1039ec52562b7d79d51731cbf483;"
+                // "Database=d97hs6fcue95i0;"
+                // "SslMode=Require;"
+                // "TrustServerCertificate=true;"
+
+                
+            }
 
             services.AddScoped<ITokenService, TokenService>();
-            services.AddDbContext<UserContext>(options => options.UseNpgsql(Configuration.GetConnectionString(connStr)));
-            services.AddDbContext<VisitorContext>(options => options.UseNpgsql(Configuration.GetConnectionString(connStr)));
+
+            services.AddDbContext<UserContext>(options => options.UseNpgsql(connStr));
+            services.AddDbContext<VisitorContext>(options => options.UseNpgsql(connStr));
             // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
         
