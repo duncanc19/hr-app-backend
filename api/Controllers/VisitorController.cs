eec046b6043bc60e4b69cf7f17b9d0401a688621
duncanc19 <duncancarter93@gmail.com>
2020-11-27 10:02:02 +0000

    Add authorize attribute to visitor controller

--- api/Controllers/VisitorController.cs
+++ api/Controllers/VisitorController.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using HRApp.API.Models;
-using System.Reflection;
 using Microsoft.AspNetCore.Cors;
 
 namespace HRApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     [EnableCors("AllowOrigin")]
